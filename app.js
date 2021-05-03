668533099f3f3e08337d5f00c3f964afc633880e
DanielSuhett <danysuhett@hotmail.com>
2021-05-02 22:36:39 -0300

    fix por server

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ require("./middleware")(app, express);
 require("./routes")(app);
 
 if (process.env.NODE_ENV !== "test")
-  app.listen(3001, () => {
-    console.log(`Server on 3001`);
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server on ${process.env.PORT || 3000}`);
   });
 
 module.exports = app;
